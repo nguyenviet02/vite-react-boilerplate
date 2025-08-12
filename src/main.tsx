e1d7083b1d4ed8bcae49856dd6a231bdf622084f
Nguyen Viet <nguyenviet02.dev@gmail.com>
2025-08-12 15:40:31 +0700

    fix: i18n

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { routeTree } from "./routeTree.gen.ts";
 import "./styles/tailwind.css";
-import "./common/i18n";
 
 const router = createRouter({ routeTree });
 
